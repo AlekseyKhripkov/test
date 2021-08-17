7a55ad33e0e59d41a3566a7c396e1621260cddef
kirill.rumynin <krumynin1993@gmail.com>
2021-08-17 18:22:10 +0300

    типизация

--- index.php
+++ index.php
@@ -2,27 +2,30 @@
 
 class Human
 {
-    private $name;
-    private $surname;
-    private $patronymic;
-    private $date;
-    private $relatives = [];
+    private string $name;
+    private string $surname;
+    private string $patronymic;
+    private DateTime $date;
+    private array $relatives = [];
 
-    public function __construct($name, $surname, $patronymic, $date, $relatives = [])
-    {
+    public function __construct(
+        string $name,
+        string $surname,
+        string $patronymic,
+        DateTime $date
+    ) {
         $this->name = $name;
         $this->surname = $surname;
         $this->patronymic = $patronymic;
         $this->date = $date;
-        $this->relatives = $relatives;
     }
 
-    public function getName()
+    public function getName(): string
     {
         return $this->surname . " " . substr($this->name, 0, 1) . "." . substr($this->patronymic, 0, 1) . ".";
     }
 
-    public function addRelatives($a)
+    public function addRelatives(Human $a): void
     {
         for ($i = 0; $i < count($this->relatives); $i++) {
             if ($this->relatives[$i]->isEqual($a)) {
@@ -33,17 +36,17 @@ class Human
         $a->addRelatives($this);
     }
 
-    public function getRelatives()
+    public function getRelatives(): array
     {
         return $this->relatives;
     }
 
-    public function getDate()
+    public function getDate(): DateTime
     {
         return $this->date;
     }
 
-    public function isAdult()
+    public function isAdult(): bool
     {
         $now = new DateTime(); // текущее время
         $dateDiff = $this->date->diff($now); // рзаница текущей даты и даты обьекта
@@ -51,7 +54,7 @@ class Human
         return $dateDiff->y > 18;
     }
 
-    private function isEqual ($a)
+    private function isEqual(Human $a): bool
     {
         if($this->name == $a->name){
             return true;
@@ -70,30 +73,33 @@ $sokolov = new Human("Petr" , "Sokolov" , "Ivanovich" , new DateTime(1992-07-24)
 $perepechka = new Human("Sergey" , "Perepechka" , "Egorovich" , new DateTime(2009-06-21));
 
 
-$petrov->addRelatives($ivanov);
-$petrov->addRelatives($sidorov);
-$petrov->addRelatives($sokolov);
 
+echo "123";
 
-$relatives = $petrov->getRelatives();
-$isAdultRelatives = [];
-$isNotAdultRelatives = [];
-foreach ($relatives as $relative) {
-    if ($relative->isAdult()) {
-        $isAdultRelatives[] = $relative;
-    } else {
-        $isNotAdultRelatives[] = $relative;
-    }
-}
-
-echo "Старые родственники: \n";
-foreach ($isAdultRelatives as $relative) {
-    echo $relative->getName() . "\n";
-}
-
-echo "\n\n";
-
-echo "Молодые родственники: \n";
-foreach ($isNotAdultRelatives as $relative) {
-    echo $relative->getName() . "\n";
-}
+//$petrov->addRelatives($ivanov);
+//$petrov->addRelatives($sidorov);
+//$petrov->addRelatives($sokolov);
+//
+//
+//$relatives = $petrov->getRelatives();
+//$isAdultRelatives = [];
+//$isNotAdultRelatives = [];
+//foreach ($relatives as $relative) {
+//    if ($relative->isAdult()) {
+//        $isAdultRelatives[] = $relative;
+//    } else {
+//        $isNotAdultRelatives[] = $relative;
+//    }
+//}
+//
+//echo "Старые родственники: \n";
+//foreach ($isAdultRelatives as $relative) {
+//    echo $relative->getName() . "\n";
+//}
+//
+//echo "\n\n";
+//
+//echo "Молодые родственники: \n";
+//foreach ($isNotAdultRelatives as $relative) {
+//    echo $relative->getName() . "\n";
+//}
